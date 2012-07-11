d10f65fa20d86a87754db76e32babdc931b48e2c
Denis Costa <deniscostadsc@gmail.com>
2012-07-11 20:14:44 -0300

    Começando o projeto

--- twitter/twitter.py
+++ twitter/twitter.py
@@ -1,1 +1,26 @@
+# coding: utf-8
+
 from pymongo import Connection
+import tweepy
+
+doencas = ['dengue', u'malária']
+
+api = tweepy.API()
+
+connection = Connection('localhost', 27017)
+
+raw_tweets_db = connection['raw_tweets']
+
+posts = raw_tweets_db.posts
+
+for doenca in doencas:
+    tweets = api.search(q=doenca, rpp=100)
+    for tweet in tweets:
+        t = {
+            'created_at': tweet.created_at,
+            'from_user_id': tweet.from_user_id,
+            'text' : tweet.text,
+            'geo': tweet.geo,
+        }
+
+        posts.insert(t)
