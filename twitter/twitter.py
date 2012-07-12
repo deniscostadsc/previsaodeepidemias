6698803d197239dc351592b11989b5e3a10115d4
Denis Costa <deniscostadsc@gmail.com>
2012-07-12 09:50:19 -0300

    Agora o ID do tweet é único.

--- twitter/twitter.py
+++ twitter/twitter.py
@@ -11,6 +11,8 @@ connection = Connection('localhost', 27017)
 
 db = connection['TG']
 
+db.raw_tweets.ensure_index('id', unique=True)
+
 raw_tweets = db.raw_tweets
 
 for doenca in doencas:
@@ -18,6 +20,7 @@ for doenca in doencas:
     for tweet in tweets:
         t = {
             'created_at': tweet.created_at,
+            'id': tweet.id,
             'from_user_id': tweet.from_user_id,
             'text' : tweet.text,
             'geo': tweet.geo,
