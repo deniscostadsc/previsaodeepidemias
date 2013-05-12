7fe9995ed89e02913af8f43bdd0cb3ff9fe5b0e5
Denis Costa <deniscostadsc@gmail.com>
2013-05-11 22:53:29 -0300

    simplificando projeto

--- src/get_tweets.py
+++ src/get_tweets.py
@@ -1,5 +1,32 @@
-from twitter.twitter import get_tweets
-from utils import doencas
+#!/usr/bin/env python
+# coding: utf-8
+
+from pymongo import Connection
+import tweepy
+
+doencas = [
+    'dengue',
+    'gripe',
+    'tuberculose',
+    u'cólera',
+    u'doença de chagas',
+    u'malária',
+]
+
+
+def get_tweets(terms):
+    api = tweepy.API()
+
+    connection = Connection('localhost', 27017)
+    db = connection['TG']
+    db.raw_tweets.ensure_index('id', unique=True)
+
+    raw_tweets = db.raw_tweets
+
+    for term in terms:
+        tweets = api.search(q=term, rpp=100)
+        for tweet in tweets:
+            raw_tweets.insert(tweet)
 
 if __name__ == '__main__':
     get_tweets(doencas)
