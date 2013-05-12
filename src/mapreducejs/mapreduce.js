e6d30bca58af4f3e0decba779a552be2bd7e1618
Denis Costa <deniscostadsc@gmail.com>
2013-05-11 22:59:08 -0300

    Removendo código não utilizado.

--- src/mapreducejs/mapreduce.js
+++
@@ -1,9 +0,0 @@
-var diseases = ["dengue", "gripe", "tuberculose", "cólera", "doença de chagas", "malária"];
-var diseases_re = [/dengue/i, /gripe/i, /tuberculose/i, /c[oó]lera/i, /doen[cç]a de chagas/i, /mal[aá]ria/i];
-
-var map = function(text){
-    for (var i=0; disease_re.length; i++) {
-        if (disease_re.exec(text))
-            return {}
-    };
-
