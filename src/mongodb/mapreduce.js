83dd89a0fdb7e55bcd99de26cd7b1e016350bd6e
Denis Costa <deniscostadsc@gmail.com>
2013-06-11 10:48:03 -0300

    Adicionando código inicial do mapreduce.

---
+++ src/mongodb/mapreduce.js
@@ -0,0 +1,49 @@
+use previsaodeepidemias;
+
+map = function (){
+    var first;
+    for (var disease in diseases){
+        if (diseases[disease].test(this.text)){
+            emit({
+                disease: disease,
+                week_start: firstDayOfWeek(this.created_at),
+                location: this.location,
+                location_str: this.location_str
+            }, {
+                count: 1
+            });
+        }
+    }
+};
+
+reduce = function (key, emits){
+    for (var i in emits){
+        total += emits[i].count;
+    }
+    return {count: total};
+};
+
+
+db.raw_tweets.mapReduce(
+    map,
+    reduce,
+    {
+        keeptemp: true,
+        out: 'diseaseAnalysis',
+        verbose: true,
+        scope: {
+            diseases: {
+                'dengue': /dengue/i,
+                'gripe': /h1n1|grip(e|ado|ada)/i,
+                'tuberculose': /tuberculos[eoa]/i,
+                'colera': /c[oó]lera/i,
+                'malaria': /mal[aá]ria/i
+            },
+
+            firstDayOfWeek: function (date){
+                week = new Date(date.setDate(date.getDate() - date.getDay()));
+                return new Date(week.getDate() + '-' + week.getMonth() + '-' + week.getYear());
+            }
+        }
+    }
+);
